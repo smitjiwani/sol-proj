a0a9762d59bdb0fc62d9c0601b078f6130f2a3af
smitjiwani <smitjiwani44@gmail.com>
2024-04-03 22:55:23 +0530

    readme updated and user contrct added

--- README.md
+++ README.md
@@ -3,3 +3,42 @@ cargo: 1.77.1
 anchor-cli 0.29.0
 
 https://www.soldev.app/course/local-setup
+
+## Frontend 
+
+Put it in /fanasy/app
+
+## Setup 
+
+download rust
+
+```
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+download solana cli
+
+```
+sh -c "$(curl -sSfL https://release.solana.com/v1.16.4/install)"
+```
+
+```
+solana config set --url localhost
+solana-keygen new -o /<your username>/.config/solana/id.json
+solana airdrop 2
+solana balance
+```
+
+```
+cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
+```
+
+```
+avm install latest
+avm use latest
+```
+
+```
+anchor --version
+```
+
--- fantasy/programs/fantasy/src/lib.rs
+++ fantasy/programs/fantasy/src/lib.rs
@@ -1,13 +1,32 @@
 use anchor_lang::prelude::*;
 
+#![feature(proc_macro_hygiene)]
+#![allow(unused_attributes)]
+
+
+#[program]
+mod user_contract;
+
 declare_id!("GtyvgHNxNFrbSf4AJSiwsJjUqvmE7vLCykfoV7oRPicV");
 
 #[program]
 pub mod fantasy {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn initialize(ctx: Context<Empty>) -> ProgramResult {
+        user_contract::UserContract::new(ctx)
+    }
+
+    pub fn create_user(ctx: Context<Empty>, team_name: String) -> ProgramResult {
+        user_contract::UserContract::create_user(ctx, team_name)
+    }
+
+    pub fn add_player(ctx: Context<Empty>, user_index: usize, player_name: String) -> ProgramResult {
+        user_contract::UserContract::add_player(ctx, user_index, player_name)
+    }
+
+    pub fn get_users(ctx: Context<Empty>) -> ProgramResult {
+        user_contract::UserContract::get_users(ctx)
     }
 }
 
